cbdafd96f5684461f4bf9c915508f7a58f216c6b
hoangminhas <hoangminh9973@gmail.com>
2022-03-29 01:10:57 +0700

    tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 5, name: 'Watchmen'},
+  {id: 6, name: 'Wolverine'},
+  {id: 7, name: 'Super boy'},
+  {id: 8, name: 'Spider girl'},
+  {id: 9, name: 'Sandman'},
+  {id: 10, name: 'Super QA'},
+  {id: 11, name: 'Professor Z'},
+  {id: 12, name: 'The beetle'},
+  {id: 13, name: 'John Mccartney'},
+  {id: 14, name: 'Bronze man'},
+  {id: 15, name: 'Badman'},
+];
